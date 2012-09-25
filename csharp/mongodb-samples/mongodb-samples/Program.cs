73fbb5ee1c0750aa2f9bc24d47d54394d41ffcc1
Emilio Torrens <emilio.torrens@etooltech.com>
2012-09-25 18:30:48 +0200

    Ejemplos C# para MallorcaMUG

---
+++ csharp/mongodb-samples/mongodb-samples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mongodb_samples
+{
+    
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Child> Chidls { get; set; }
+    }
+
+    public class Child
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
