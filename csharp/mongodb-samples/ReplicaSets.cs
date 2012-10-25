d5dd65db9f3010ff269ec69bedc33b877c6269b9
Emilio Torrens <emilio.torrens@etooltech.com>
2012-10-25 16:10:54 +0200

    Replica Sets

---
+++ csharp/mongodb-samples/ReplicaSets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace mongodb_samples
+{
+    [TestFixture]
+    public class ReplicaSets
+    {
+        [Test]
+        public void Test()
+        {
+            var server = MongoServer.Create("mongodb://192.168.1.218/?replicaSet=devSet;connect=automatic");
+            server.Connect();
+            Assert.AreEqual(3,server.Instances.Count());            
+
+            //BreakPoint para hacer que caiga el primario (218)
+            int i = 0;
+
+            var database = server.GetDatabase("testdb");
+            var collection = database.GetCollection<Person>("persons");
+            collection.RemoveAll();
+
+
+        }
+    }
+}
--- python/replicasets.py
+++ python/replicasets.py
@@ -1,1 +1,7 @@
 __author__ = 'emilio.torrens'
+
+import pymongo
+
+connection = pymongo.ReplicaSetConnection('192.168.1.218:27017', replicaSet='devSet')
+instances = connection.instances
+print instances
