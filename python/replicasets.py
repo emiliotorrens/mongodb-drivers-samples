a8eb4575e0029e834558e535b1d0b77aff160c43
Emilio Torrens <emilio.torrens@etooltech.com>
2012-10-25 17:02:52 +0200

    Replica Sets

--- python/replicasets.py
+++ python/replicasets.py
@@ -2,6 +2,6 @@ __author__ = 'emilio.torrens'
 
 import pymongo
 
-connection = pymongo.ReplicaSetConnection('192.168.1.218:27017', replicaSet='devSet')
+connection = pymongo.ReplicaSetConnection('192.168.1.69:27017', replicaSet='mySet')
 instances = connection.instances
 print instances
